bcd54ea1daf673032536c6512e308ba768e2cd2c
ZINBOYZ <nathanlops1228@gmail.com>
2026-09-23 08:58:21 -0400

    Lightswitch modelling

---
+++ Assets/Scripts/lightswitch.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class lightswitch : MonoBehaviour
+{
+    public GameObject intIcon, lightOn, lightOff, switchOn, switchOff;
+    public bool toggle;
+    public AudioSource switchSound;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (toggle == true)
+                {
+                    lightOn.SetActive(true);
+                    lightOff.SetActive(false);
+                    switchOn.SetActive(true);
+                    switchOff.SetActive(false);
+                    //switchSound.Play()
+                }
+                if (toggle == false)
+                {
+                    lightOn.SetActive(false);
+                    lightOff.SetActive(true);
+                    switchOn.SetActive(false);
+                    switchOff.SetActive(true);
+                    //switchSound.Play();
+                }
+            }
+        }
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            intIcon.SetActive(false);
+        }
+    }
+}
